b4376ef8cd15a34a2fa21184359314c37c932001
che2n3jigw <che2n3jigw@163.com>
2026-04-09 17:11:26 +0800

    feat(me): 实现我的页面布局(静态)

--- feature/me/impl/src/main/kotlin/com/che2n3jigw/naviplayer/feature/me/impl/MeFragment.kt
+++ feature/me/impl/src/main/kotlin/com/che2n3jigw/naviplayer/feature/me/impl/MeFragment.kt
@@ -20,6 +20,9 @@
 // 创建时间： 2026/4/1 16:19
 package com.che2n3jigw.naviplayer.feature.me.impl
 
+import android.view.ViewGroup
+import androidx.core.graphics.Insets
+import androidx.core.view.updateLayoutParams
 import com.che2n3jigw.naviplayer.core.ui.BaseFragment
 import com.che2n3jigw.naviplayer.feature.me.impl.databinding.FragmentMeBinding
 
@@ -30,9 +33,28 @@ class MeFragment : BaseFragment<FragmentMeBinding>() {
 
     override fun inflateBinding() = FragmentMeBinding.inflate(layoutInflater)
 
-    override fun initView() {}
+    override fun initView() {
+        binding.ivAvatar.setImageResource(R.drawable.me_placeholder_avatar)
+        binding.viewStatAlbums.setData(R.drawable.me_ic_albums, "10000", "Albums")
+        binding.viewStatArtists.setData(R.drawable.me_ic_artists, "14", "Artists")
+        binding.viewStatLists.setData(R.drawable.me_ic_lists, "10", "Lists")
+        binding.viewStatOffline.setData(R.drawable.me_ic_offline, "2.4G", "Offline")
+        binding.tvTrackCount.text = getString(R.string.me_library_tracks_count, 1081)
+        binding.ivFavoriteCover.setImageResource(R.drawable.me_placeholder_favourite_song)
+        binding.tvActive.text = getString(R.string.me_library_active, "2 分钟")
+
+        binding.ivCoverPlaying.setImageResource(R.drawable.me_placeholder_avatar)
+        binding.tvSingName.text = "sing name"
+        binding.tvSinger.text = "singer"
+    }
 
     override fun initListener() {}
 
     override fun subscribeUI() {}
+
+    override fun onApplyWindowInsets(insets: Insets) {
+        binding.ivAvatar.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            topMargin = insets.top
+        }
+    }
 }
