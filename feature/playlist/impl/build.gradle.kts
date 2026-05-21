84cc2134bf1c243c194c711dfaf77001bbd3fa0c
che2n3jigw <che2n3jigw@163.com>
2026-05-21 14:07:50 +0800

    feat(feature/playlist): implement PlaylistFragment and navigation logic in playlist:impl module

--- feature/playlist/impl/build.gradle.kts
+++ feature/playlist/impl/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     alias(libs.plugins.naviplayer.android.library)
+    alias(libs.plugins.naviplayer.hilt)
 }
 
 android {
     namespace = "com.che2n3jigw.naviplayer.feature.player.impl"
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation(projects.core.ui)
+
+    implementation(projects.feature.playlist.api)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
 }
