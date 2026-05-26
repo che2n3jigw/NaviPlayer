f0c3a2662733b9be9b162ecae4988ab4a2f4cd21
che2n3jigw <che2n3jigw@163.com>
2026-05-26 11:32:39 +0800

    feat(feature/album): initialize api and impl modules for album feature

---
+++ feature/album/impl/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.naviplayer.android.library)
+    alias(libs.plugins.naviplayer.hilt)
+}
+
+android {
+    namespace = "com.che2n3jigw.naviplayer.feature.album.impl"
+}
+
+dependencies {
+}
