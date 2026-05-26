609888032ce83e70642d50e1136c382ce48c9d24
che2n3jigw <che2n3jigw@163.com>
2026-05-26 22:18:22 +0800

    feat(feature/album): define AlbumDeepLink and AlbumNavigator interface in API module

---
+++ feature/album/api/src/main/kotlin/com/che2n3jigw/naviplayer/feature/album/api/AlbumDeepLink.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 che2n3jigw.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// 作者: che2n3jigw
+// 邮箱: che2n3jigw@163.com
+// 博客: che2n3jigw.github.io
+// 创建时间： 2026/4/1 15:03
+package com.che2n3jigw.naviplayer.feature.album.api
+
+object AlbumDeepLink {
+    /**
+     * 专辑列表页面
+     */
+    const val ALBUM_LIST = "naviplayer://feature/album/list"
+
+    /**
+     * 专辑详情
+     */
+    const val ALBUM_DETAIL = "naviplayer://feature/album/detail/{id}"
+}
